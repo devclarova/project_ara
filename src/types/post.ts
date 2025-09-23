ed0eeb30a8d67cbbd3e5a80cc885592d741404c5
dev.clarova <dev.clarova@gmail.com>
2025-09-23 18:13:28 +0900

    [docs] 게시판 DB 카테고리 수정 및 게시판 작업 중

--- src/types/post.ts
+++ src/types/post.ts
@@ -2,15 +2,16 @@ import type { Database } from './database';
 
 export type Post = Database['public']['Tables']['posts']['Row'];
 export type PostInsert = Database['public']['Tables']['posts']['Insert'] & {
-  user_id?: number;
-  category?: string | null;
-  comments?: number | null;
-  content?: string | null;
-  created_at?: string | null;
-  like?: number | null;
-  title?: string | null;
-  unlike?: number | null;
-  updated_at?: string | null;
-  view?: number | null;
+  category: string;
+  comments: number | null;
+  content: string;
+  created_at: string;
+  id: number;
+  like: number;
+  title: string;
+  unlike: number;
+  updated_at: string | null;
+  user_id: number;
+  view: number;
 };
 export type PostUpdate = Database['public']['Tables']['posts']['Update'];
