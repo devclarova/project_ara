21308dd3965edd05a00d532d93c86b236cfd2038
dev.clarova <dev.clarova@gmail.com>
2025-09-25 14:23:14 +0900

    [docs] App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -19,67 +19,57 @@ import ProfilePage from './pages/ProfilePage';
 import StudyPage from './pages/StudyPage';
 import VocaPage from './pages/VocaPage';
 import CommunityWritePage from './pages/CommunityWritePage';
-import CommunityDetailPage from './pages/CommunityDetailPage';
 import NotFound from './pages/NotFound';
 import StudyListPage from './pages/StudyListPage';
 import { PostProvider } from './contexts/PostContext';
 import CommunityListPage from './pages/CommunityListPage';
-import CommunityWritePage from './pages/CommunityWritePage';
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
 import LearningPage, { InflearnNav } from './pages/LearningPage';
-import NotFound from './pages/NotFound';
-import ProfilePage from './pages/ProfilePage';
-import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignupPage';
-import StudyListPage from './pages/StudyListPage';
-import VocaPage from './pages/VocaPage';
 import TopHeader from './components/TopHeader';
+import SignUpPage from './pages/SignUpPage';
 
 const App = () => {
   return (
     <AuthProvider>
-     <PostProvider>
-      <Router>
-        <div className="layout min-h-screen flex flex-col">
-          {/* 공통 헤더 */}
-          <TopHeader />
+      <PostProvider>
+        <Router>
+          <div className="layout min-h-screen flex flex-col">
+            {/* 공통 헤더 */}
+            <TopHeader />
 
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<LandingPage />}></Route>
-              <Route path="/landing" element={<LandingPage />}></Route>
-              <Route path="/home" element={<HomePage />}></Route>
-              <Route path="/signup" element={<SignUpPage />}></Route>
-              <Route path="/signin" element={<SignInPage />}></Route>
-              <Route path="/profile" element={<ProfilePage />}></Route>
-              <Route path="/study" element={<StudyPage />}></Route>
-              <Route path="/studyList" element={<LearningPage />}></Route>
-              <Route path="/studyList/:id" element={<StudyListPage />}></Route>
-              <Route path="/voca" element={<VocaPage />}></Route>
-              <Route path="/communitywrite" element={<CommunityWritePage />}></Route>
-              <Route path="/communitylist" element={<CommunityListPage />}></Route>
-              <Route path="/communitydetail" element={<CommunityDetailPage />}></Route>
-              <Route path="/notfound" element={<NotFound />}></Route>
-            </Routes>
-          </main>
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<LandingPage />}></Route>
+                <Route path="/landing" element={<LandingPage />}></Route>
+                <Route path="/home" element={<HomePage />}></Route>
+                <Route path="/signup" element={<SignUpPage />}></Route>
+                <Route path="/signin" element={<SignInPage />}></Route>
+                <Route path="/profile" element={<ProfilePage />}></Route>
+                <Route path="/study" element={<StudyPage />}></Route>
+                <Route path="/studyList" element={<LearningPage />}></Route>
+                <Route path="/studyList/:id" element={<StudyListPage />}></Route>
+                <Route path="/voca" element={<VocaPage />}></Route>
+                <Route path="/communitywrite" element={<CommunityWritePage />}></Route>
+                <Route path="/communitylist" element={<CommunityListPage />}></Route>
+                <Route path="/communitydetail" element={<CommunityDetailPage />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+              </Routes>
+            </main>
 
-          {/* <Footer /> */}
-          <div className={`${styles.footer} mb-16 md:mb-0`}>
-            <div className={styles.footerContent}>
-              <img className={styles.textLogo} src={textLogo} alt="Foodit" />
-              <span>서비스 이용약관 | 개인정보 처리방침</span>
-
+            {/* <Footer /> */}
+            <div className={`${styles.footer} mb-16 md:mb-0`}>
+              <div className={styles.footerContent}>
+                <img className={styles.textLogo} src={textLogo} alt="Foodit" />
+                <span>서비스 이용약관 | 개인정보 처리방침</span>
+              </div>
+              {/* 햄버거 */}
+              {/* <div className="h-[calc(4rem+env(safe-area-inset-bottom))] md:hidden" aria-hidden /> */}
             </div>
-            {/* 햄버거 */}
-            {/* <div className="h-[calc(4rem+env(safe-area-inset-bottom))] md:hidden" aria-hidden /> */}
           </div>
-
-        </div>
-        <InflearnNav />
-       </Router>
+          <InflearnNav />
+        </Router>
       </PostProvider>
-
     </AuthProvider>
   );
 };
